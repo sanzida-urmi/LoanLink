35c7062237c9255cb742fd9ec7a50cc128e69cd2
Sanzida Urmi <sanzidaurmi2002@gmail.com>
2025-12-12 01:21:11 +0600

    Add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,29 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/route.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+// import { HelmetProvider } from 'react-helmet-async'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+       <QueryClientProvider client={queryClient}>
+        {/* <HelmetProvider> */}
     <AuthProvider>
      <RouterProvider router={router} />
      </AuthProvider>
+     <Toaster></Toaster>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {/* </HelmetProvider> */}
+        </QueryClientProvider>
+
   </StrictMode>,
 )
