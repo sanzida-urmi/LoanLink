a1c330d96283ccc603b855abe0dae815237319bf
Sanzida Urmi <sanzidaurmi2002@gmail.com>
2025-12-12 01:20:23 +0600

    Add ErrorPage.jsx

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,5 +1,5 @@
 // import Button from '../components/Shared/Button/Button'
-import { useNavigate } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import Button from '../Component/Shared/Button'
 
 const ErrorPage = () => {
@@ -9,7 +9,7 @@ const ErrorPage = () => {
     <section className='bg-white '>
       <div className='container flex items-center min-h-screen px-6 py-12 mx-auto'>
         <div className='flex flex-col items-center max-w-sm mx-auto text-center'>
-          <p className='p-3 text-sm font-medium text-lime-500 rounded-full bg-blue-50 '>
+          <p className='p-3 text-sm font-medium text-sky-500 rounded-full bg-blue-50 '>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -26,34 +26,15 @@ const ErrorPage = () => {
             </svg>
           </p>
           <h1 className='mt-3 text-2xl font-semibold text-gray-800  md:text-3xl'>
-            Something Went Wrong!
+            Oop! Something Went Wrong.
           </h1>
-          <p className='mt-4 text-gray-500 '>Here are some helpful links:</p>
+          <p className='mt-4 text-gray-500 '>We couldn't load the page you're looking for</p>
 
           <div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
-            <button
-              onClick={() => navigate(-1)}
-              className='flex items-center justify-center w-1/2 px-5 py-1 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto   hover:bg-gray-100 '
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='w-5 h-5 rtl:rotate-180 text-lime-500'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
-                />
-              </svg>
-
-              <span>Go back</span>
+            <button className='btn btn-info'>
+              <Link to='/'>Go Home</Link>
             </button>
-
-            <Button label={'Take Me Home'} onClick={() => navigate('/')} />
+                       {/* <Button label={'Take Me Home'} onClick={() => navigate('/')} /> */}
           </div>
         </div>
       </div>
